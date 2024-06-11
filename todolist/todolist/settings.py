d89423e91142d53945a4be178e302b64b29100da
harsh_dadwal <harshdadymon@gmail.com>
2024-06-11 18:06:01 +0530

    Added the register app framework

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "todolistapp.apps.TodolistappConfig"
+    "todolistapp.apps.TodolistappConfig",
+    "register.apps.RegisterConfig",
 ]
 
 MIDDLEWARE = [
--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from register import views as register_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("todolistapp.urls"))
+    path("", include("todolistapp.urls")),
+    path("register/",register_views.register,name="register"),
 ]
