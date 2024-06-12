f6fb226d7ae3d6ba60691705a982eac24bae5f82
harsh_dadwal <harshdadymon@gmail.com>
2024-06-12 12:29:08 +0530

    Add Crispy forms to beautify forms [pip3 install django-crispy-forms] [pip3 install crispy-bootstrap4]

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "todolistapp.apps.TodolistappConfig",
     "register.apps.RegisterConfig",
 ]
@@ -123,3 +125,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
